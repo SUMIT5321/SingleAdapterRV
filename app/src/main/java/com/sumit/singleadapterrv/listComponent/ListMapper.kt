28e69058d52c2fc04e031f555b674a36c3d876f3
Sumit Raj <sumit.rajrn@gmail.com>
2022-12-17 08:50:36 +0530

    fixes issue

--- app/src/main/java/com/sumit/singleadapterrv/listComponent/ListMapper.kt
+++ app/src/main/java/com/sumit/singleadapterrv/listComponent/ListMapper.kt
@@ -8,7 +8,6 @@ enum class ListMapper(val resourceId: Int, val viewHolderCreator: (View) -> Cont
     SEPARATOR_VIEW(R.layout.item_view_separator, { SeparatorViewHolder(it) }),
     VIEW_ONE_VIEW(R.layout.item_view_one, { ViewOneViewHolder(it) }),
     VIEW_TWO_VIEW(R.layout.item_view_two, { ViewTwoViewHolder(it) }),
-    PAYMENT_VIEW_VIEW(R.layout.item_payment_view, { PaymentViewViewHolder(it) }),
     ;
     companion object {
         val map: Map<Int, ListMapper> = values().associateBy { it.resourceId }
